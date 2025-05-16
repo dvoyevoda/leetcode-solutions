56d57739741e17530effe2b359fc53db0b688e74
Daniel Voyevoda <danielvoyevoda@outlook.com>
2025-05-15 22:16:41 -0400

    Time: 19 ms (14.02%) | Memory: 17.8 MB (80.92%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
@@ -3,9 +3,10 @@ class Solution:
         for word in words:
             word_length = len(word)
             is_Palindrome = True
-            for i in range (word_length):
+            for i in range (word_length // 2):
                 if word[i] != word[word_length-i-1]:
                     is_Palindrome = False
+                    break
             if is_Palindrome:
                 return word
         return ""
