b156474043c83638c0068752fead2209e8898fba
Daniel Voyevoda <danielvoyevoda@outlook.com>
2025-05-16 20:16:19 -0400

    Time: 0 ms (100.00%) | Memory: 17.5 MB (99.44%) - LeetSync

--- 3312-number-of-changing-keys/number-of-changing-keys.py
+++ 3312-number-of-changing-keys/number-of-changing-keys.py
@@ -3,8 +3,7 @@ class Solution:
         current_key = s[0]
         count = 0
         for char in s:
-            if char != current_key and char.lower() != current_key:
-                if char != current_key.lower() and char.lower() != current_key:
+            if char.lower() != current_key.lower():
                     count += 1
             current_key = char
         return count
